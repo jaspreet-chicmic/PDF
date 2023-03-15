37e3b1957d4944e0da8891ba6f1de5243dd047a1
jaspreet-chicmic <jaspreet.singh@chicmic.co.in>
2023-03-15 16:46:23 +0530

    tested-isPdf-and-other-PdfJS-functions

---
+++ pdf-react-application/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Pdf from './Pdf';
+
+function App() {
+  return (
+    <div className="App">
+      <Pdf/>
+    </div>
+  );
+}
+
+export default App;
